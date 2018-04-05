88049071dfe3944094cb869c78334237e1f99906
Elaine Venancio <elaine.martucheli@gmail.com>
2018-04-05 20:03:48 +0100

    testing database products

--- src/main/java/org/academiadecodigo/hackathon/App.java
+++ src/main/java/org/academiadecodigo/hackathon/App.java
@@ -3,6 +3,10 @@ package org.academiadecodigo.hackathon;
 import org.academiadecodigo.hackathon.model.products.Category;
 import org.academiadecodigo.hackathon.model.products.Product;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class App {
 
     public static void main(String[] args) {
@@ -14,5 +18,14 @@ public class App {
         product.setName("milho");
 
         category.addProduct(product);
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("simone");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        em.persist(category);
+        em.getTransaction().commit();
+        em.close();
+
     }
 }
