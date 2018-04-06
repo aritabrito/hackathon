b8cd40cae341bff756973c259fc5da583ad11980
RafaelVCoutinho <rafaelveigacoutinho@gmail.com>
2018-04-06 09:19:54 +0100

    bootstrap done

--- src/main/java/org/academiadecodigo/hackathon/App.java
+++ src/main/java/org/academiadecodigo/hackathon/App.java
@@ -21,73 +21,8 @@ public class App extends Application {
 
     public static void main(String[] args) {
 
-        User user1 = new User();
-        user1.setFirstName("rita");
-        user1.setLastName("brito");
-        user1.setUsername("rb");
-        user1.setEmail("oi");
-        user1.setPassword("oi");
-
-        User user2 = new User();
-        user2.setFirstName("cat");
-        user2.setLastName("mach");
-        user2.setUsername("cm");
-        user2.setEmail("oi");
-        user2.setPassword("oi");
-
-
-        Category category1 = new Category();
-        category1.setName("food");
-
-        Category category2 = new Category();
-        category2.setName("vehicles");
-
-        Category category3 = new Category();
-        category3.setName("animals");
-
-
-        Product product1 = new Product();
-        product1.setName("corn");
-
-        Product product2 = new Product();
-        product2.setName("tractor");
-
-        Product product3 = new Product();
-        product3.setName("sheep");
-
-        category1.addProduct(product1);
-        category1.addProduct(product2);
-        category1.addProduct(product3);
-
-
-        JpaSessionManager sessionManager = new JpaSessionManager();
-        JpaTransactionManager transactionManager = new JpaTransactionManager(sessionManager);
-
-        JpaUserDao userDao = new JpaUserDao(sessionManager);
-        JpaProductDao productDao = new JpaProductDao(sessionManager);
-        JpaCategoryDao jpaCategoryDao = new JpaCategoryDao(sessionManager);
-        JpaUserService userService = new JpaUserService(transactionManager, userDao);
-        JpaProductService productService = new JpaProductService(transactionManager, productDao);
-        JpaCategoryService jpaCategoryService = new JpaCategoryService(transactionManager, jpaCategoryDao);
-
-
-        JpaAuthenticateLogin jpaAuthenticateLogin = new JpaAuthenticateLogin(transactionManager, userDao);
-
-
-
-        ServiceRegistry.getServiceRegistry().registerService(UserService.class.getSimpleName(), userService);
-        ServiceRegistry.getServiceRegistry().registerService(CategoryService.class.getSimpleName(), jpaCategoryService);
-        ServiceRegistry.getServiceRegistry().registerService(ProductService.class.getSimpleName(), productService);
-        ServiceRegistry.getServiceRegistry().registerService(JpaAuthenticateLogin.class.getSimpleName(), jpaAuthenticateLogin);
-
-
-
-        userService.save(user1);
-        userService.save(user2);
-        jpaCategoryService.save(category1);
-        jpaCategoryService.save(category2);
-        jpaCategoryService.save(category3);
-
+        Bootstrap bootstrap = new Bootstrap();
+        bootstrap.init();
 
         launch(args);
 
